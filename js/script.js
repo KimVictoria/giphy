769324ffdf6704973dc9edcda9890f0a54b4cdce
KimVictoria <kimberlyvmagpantay@icloud.com>
2020-03-10 01:08:43 +0000

    Finished the project as well as added some extensions

--- js/script.js
+++ js/script.js
@@ -4,8 +4,33 @@
 /* global $ */
 
 $("#search-button").click(function(){
+
+let searchTerm = $("input").val();
   
-  
-  
+  var request_url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    
+  fetch(request_url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var pic_url = data.data[Math.floor(Math.random() * data.data.length)].images.original.url;
+      console.log(data)
+        $('.picture').html('<marquee><img src=' + pic_url + '></marquee>');
+    })
 });
+
+var trending = "https://api.giphy.com/v1/gifs/trending?rating=pg&api_key=dc6zaTOxFJmzC"
+console.log("hello")
+fetch(trending)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+    var pic_url = data.data[Math.floor(Math.random() * data.data.length)].images.original.url;
+    console.log(data)
+    $('.trendingpicture').html('<marquee><img src=' + pic_url + '></marquee>');
+    })
+    
+
 
